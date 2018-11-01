6426eac370b2567f77a7964fa3f2faac50d4fc40
gigiogigo <gigiogigo@users.noreply.github.com>
2018-10-31 20:41:22 -0500

    Update MainActivity.java

--- app/src/main/java/com/main/retrace/retrace/MainActivity.java
+++ app/src/main/java/com/main/retrace/retrace/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    // This is a test.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
